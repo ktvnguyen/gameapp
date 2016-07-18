4633981b5137fe4c347af4e9bd47431e1b704008
ktvnguyen <ktvnguyen2@gmail.com>
2016-07-18 15:33:05 +0800

    remove test change

--- GameApp/GameApp/MasterViewController.swift
+++ GameApp/GameApp/MasterViewController.swift
@@ -20,7 +20,7 @@ class MasterViewController: UITableViewController {
         self.navigationItem.leftBarButtonItem = self.editButtonItem()
         
         
-        let data = NSFileManager.defaultManager().contentsAtPath(NSBundle.mainBundle().pathForResource("data", ofType: "json")!)
+        var data = NSFileManager.defaultManager().contentsAtPath(NSBundle.mainBundle().pathForResource("data", ofType: "json")!)
         
         do{
             objects = try NSJSONSerialization.JSONObjectWithData(data!, options: .MutableContainers) as! [NSDictionary]
